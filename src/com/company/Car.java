af62715d6720ef80da33d61204f06dacac115a3b
Brian B <brianb@MacBook-Pro-4.local>
2019-01-22 13:49:51 -0500

    Adding Car class with set methods

---
+++ src/com/company/Car.java
@@ -0,0 +1,27 @@
+package com.company;
+
+public class Car {
+
+    private String color;
+    private String make;
+    private String model;
+    private int year;
+
+
+    public void setColor(String carColor){
+        color = carColor;
+    }
+
+    public void setMake(String carMake){
+        make = carMake;
+    }
+
+    public void setModel(String carModel){
+        model = carModel;
+    }
+
+    public void setYear(int carYear){
+        year = carYear;
+    }
+
+}
