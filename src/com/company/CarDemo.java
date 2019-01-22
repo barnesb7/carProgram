0db9c633a2abe6d87c508b5af0a5283ddf1cd9ed
Brian B <brianb@MacBook-Pro-4.local>
2019-01-22 13:59:57 -0500

    Adding instantiation of Car object with message to user displayed

--- src/com/company/CarDemo.java
+++ src/com/company/CarDemo.java
@@ -3,6 +3,15 @@ package com.company;
 public class CarDemo {
 
     public static void main(String[] args) {
-	// write your code here
+	    Car mycar = new Car();
+
+	    mycar.setColor("Maroon");
+	    mycar.setMake("Ford");
+	    mycar.setModel("Pinto");
+	    mycar.setYear(1971);
+
+	    String space = " ";
+
+	    System.out.println("We're sorry, but your " + mycar.getYear() + space + mycar.getColor() + space + mycar.getMake() + space + mycar.getModel() + " is too fast and much too furious.");
     }
 }
