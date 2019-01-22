20af8cea5b41f15dea5136a0728f08a3ecbdadd9
Brian B <brianb@MacBook-Pro-4.local>
2019-01-22 13:52:52 -0500

    Adding get methods

--- src/com/company/Car.java
+++ src/com/company/Car.java
@@ -12,16 +12,32 @@ public class Car {
         color = carColor;
     }
 
+    public String getColor(){
+        return color;
+    }
+
     public void setMake(String carMake){
         make = carMake;
+    }
+
+    public String getMake(){
+        return make;
     }
 
     public void setModel(String carModel){
         model = carModel;
     }
 
+    public String getModel(){
+        return model;
+    }
+
     public void setYear(int carYear){
         year = carYear;
+    }
+
+    public int getYear(){
+        return year;
     }
 
 }
